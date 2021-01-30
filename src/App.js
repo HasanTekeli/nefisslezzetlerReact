a0e64fc8cc571ef9868bf03e120dabca329be39a
Hasan A. Tekeli <hasan_tekeli@yahoo.com>
2021-01-30 17:31:45 +0300

    top and bottom nav added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Cookies from './pages/Cookies';
+import About from "./pages/About"
+
+import Navigation from "./components/Navigation"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Navigation />
+            <Switch>
+              <Route path="/home" component={Home} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/cookies" component={Cookies} />
+              <Route path="/about" component={About} />
+            </Switch>
+      </BrowserRouter>
     </div>
   );
 }
