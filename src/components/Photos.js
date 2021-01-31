51e91ff2ac3cc1eea5b34de988831b129b80f016
Hasan A. Tekeli <hasan_tekeli@yahoo.com>
2021-01-31 22:18:48 +0300

    renamed images

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,18 +1,21 @@
 export const photos = [
     {
-        src: "./assets/Products/Atom Kurabiye.jpeg",
-        width: 4,
-        height: 3
-    },
-    {
-        src: "./assets/Products/Açma.jpeg",
-        width: 4,
-        height: 3
-    },
-    {
-        src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+        src: "https://raw.githubusercontent.com/HasanTekeli/nefisslezzetlerReact/master/src/components/assets/Products/1.jpeg",
         width: 4,
         height: 3,
-        title: "Unsplash"
-    }
+        title: "Atom Kurabiye"
+    },
+    {
+        src: "https://raw.githubusercontent.com/HasanTekeli/nefisslezzetlerReact/master/src/components/assets/Products/2.jpeg",
+        width: 4,
+        height: 3,
+        title: "Açma"
+    },
+    {
+        src: "https://raw.githubusercontent.com/HasanTekeli/nefisslezzetlerReact/master/src/components/assets/Products/3.jpeg",
+        width: 4,
+        height: 3,
+        title: "Ceviz Dolgulu Kerebiç Kurabiyesi"
+    },
+
 ]
